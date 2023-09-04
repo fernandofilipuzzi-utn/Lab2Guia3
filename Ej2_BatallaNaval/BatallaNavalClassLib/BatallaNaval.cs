7d4c5ec6be1f5d166ef96771ad84bdbbd3171672
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-09-04 10:14:05 -0300

    avances

--- Ej2_BatallaNaval/BatallaNavalClassLib/BatallaNaval.cs
+++ Ej2_BatallaNaval/BatallaNavalClassLib/BatallaNaval.cs
@@ -2,31 +2,30 @@
 using System.Collections;
 
 namespace BatallaNavalClassLib
-
 {
     public class BatallaNaval
     {
         Jugador jugador1;
         Jugador jugador2;
 
-        Celda[,] mar;
-
-        public Celda this[int fila, int columna]
-        {
-            get
-            {
-                Celda celda = null;
-                if(mar!=null)
-                    celda= mar[fila, columna];
-                return celda;
-            }
-        }
-
         public int CantFilas;
         public int CantColumnas;
 
         public BatallaNaval(string nombre, int CantFilas, int CantColumnas)
         {
+            #region factoria jugador 1
+            Celda [, ]mar = new Celda[CantFilas, CantColumnas];
+            for (int n = 0; n < CantFilas; n++)
+            {
+                for (int m = 0; m < CantFilas; m++)
+                {
+                    mar[n, m] = new Celda();
+                }
+            }
+            jugador1 = new Jugador(nombre, mar);
+            #endregion
+
+            #region factoria jugador 2
             mar = new Celda[CantFilas, CantColumnas];
             for (int n = 0; n < CantFilas; n++)
             {
@@ -35,8 +34,8 @@ namespace BatallaNavalClassLib
                     mar[n, m] = new Celda();
                 }
             }
+            jugador2 = new Jugador("Máquina", mar);
+            #endregion
         }
-
-       
     }
 }
--- Ej2_BatallaNaval/BatallaNavalClassLib/Jugador.cs
+++ Ej2_BatallaNaval/BatallaNavalClassLib/Jugador.cs
@@ -13,6 +13,23 @@ namespace BatallaNavalClassLib
         public enum TipoAcierto { Agua=1, Impacto, Hundido}
         public string Nombre { get; private set; }
 
-        
+        Celda[,] mar;
+
+        public Jugador(string nombre, Celda[,] mar)
+        {
+            Nombre = nombre;
+            this.mar = mar;
+        }
+
+        public Celda this[int fila, int columna]
+        {
+            get
+            {
+                Celda celda = null;
+                if (mar != null)
+                    celda = mar[fila, columna];
+                return celda;
+            }
+        }
     }
 }
